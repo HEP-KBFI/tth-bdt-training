bdc8ab525e5bcef74d1a6ba552ebb968ea8f293f
Laurits <laurits.tani@gmail.com>
2019-12-03 09:57:08 +0100

    print out output_dir

--- scripts/quasar_tth_analysis.py
+++ scripts/quasar_tth_analysis.py
@@ -55,6 +55,7 @@ def main():
     print("\n============ Saving results ================\n")
     universal.save_results(result_dict, output_dir, plot_extras=True)
     sm.clear_from_files(global_settings)
+    print("Results saved to " + str(output_dir))
 
 
 
