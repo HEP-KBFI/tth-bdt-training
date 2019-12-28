672d5e3c9fac3116d2ea19ce6fd5416d5b881ec8
Laurits <laurits.tani@gmail.com>
2019-12-28 16:13:00 +0200

    fix

--- scripts/trainvar_opt.py
+++ scripts/trainvar_opt.py
@@ -1,8 +1,52 @@
 from tthAnalysis.bdtTraining import trainvar_choice as tc
-
+from tthAnalysis.bdtHyperparameterOptimization import pso_main as pm
+from tthAnalysis.bdtHyperparameterOptimization import xgb_tools as xt
 
 def main():
-    tc.run_trainvar_optimization()
+    global_settings = universal.read_settings('global')
+    channel = global_settings['channel']
+    nthread = global_settings['nthread']
+    bdtType = global_settings['bdtType']
+    trainvar = global_settings['trainvar']
+    output_dir = os.path.expandvars(global_settings['output_dir'])
+    cmssw_base_path = os.path.expandvars('$CMSSW_BASE')
+    param_file = os.path.join(
+        cmssw_base_path,
+        'src',
+        'tthAnalysis',
+        'bdtHyperparameterOptimization',
+        'data',
+        'xgb_parameters.json'
+    )
+    value_dicts = universal.read_parameters(param_file)
+    pso_settings = pm.read_weights()
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+    trainvars = tc.initialize_trainvars(channel)
+    tc.write_new_trainvar_list(trainvars, output_dir)
+    while len(trainvars) > 10:
+        data, trainVars = ttHxt.tth_analysis_main(
+            channel, bdtType, nthread,
+            output_dir, trainvar, tc
+        )
+        data_dict = ttHxt.createDataSet(data, trainVars, nthread)
+        print("::::::: Reading parameters :::::::")
+        parameter_dicts = xt.prepare_run_params(
+            value_dicts, pso_settings['sample_size'])
+        print("\n============ Starting hyperparameter optimization ==========\n")
+        result_dict = pm.run_pso(
+            data_dict, value_dicts, sm.run_iteration, parameter_dicts
+        )
+        feature_importances = result_dict['feature_importances']
+        named_feature_importances = tc.relate_trainvar_name_and_number(
+            feature_importances, trainvars)
+        if len(trainvars) > 10:
+            trainvars = tc.drop_worst_parameters(named_feature_importances)
+        tc.write_new_trainvar_list(trainvars, output_dir)
+    print("\n============ Saving results ================\n")
+    universal.save_results(result_dict, output_dir, plot_extras=True)
+    sm.clear_from_files(global_settings)
+    print("Results saved to " + str(output_dir))
 
 
 if __name__ == '__main__':
