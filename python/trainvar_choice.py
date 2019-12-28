1fa331f46038e33295f911e7abd022c3c6082764
Laurits <laurits.tani@gmail.com>
2019-12-28 16:05:56 +0200

    initial complete trainvar_choice

--- python/trainvar_choice.py
+++ python/trainvar_choice.py
@@ -221,7 +221,45 @@ def drop_worst_parameters(named_feature_importances):
 def run_trainvar_optimization():
     global_settings = universal.read_settings('global')
     channel = global_settings['channel']
+    nthread = global_settings['nthread']
+    bdtType = global_settings['bdtType']
+    trainvar = global_settings['trainvar']
+    output_dir = os.path.expandvars(global_settings['output_dir'])
+    cmssw_base_path = os.path.expandvars('$CMSSW_BASE')
+    param_file = os.path.join(
+        cmssw_base_path,
+        'src',
+        'tthAnalysis',
+        'bdtHyperparameterOptimization',
+        'data',
+        'xgb_parameters.json'
+    )
+    value_dicts = universal.read_parameters(param_file)
+    pso_settings = pm.read_weights()
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
     trainvars = initialize_trainvars(channel)
-
-
-# Write what sample with what path was used and which trainvars were the optimal
+    write_new_trainvar_list(trainvars, out_dir)
+    while len(trainvars) > 10:
+        data, trainVars = ttHxt.tth_analysis_main(
+            channel, bdtType, nthread,
+            output_dir, trainvar, choose_trainVar
+        )
+        data_dict = ttHxt.createDataSet(data, trainVars, nthread)
+        print("::::::: Reading parameters :::::::")
+        parameter_dicts = xt.prepare_run_params(
+            value_dicts, pso_settings['sample_size'])
+        print("\n============ Starting hyperparameter optimization ==========\n")
+        result_dict = pm.run_pso(
+            data_dict, value_dicts, sm.run_iteration, parameter_dicts
+        )
+        feature_importances = result_dict['feature_importances']
+        named_feature_importances = relate_trainvar_name_and_number(
+            feature_importances, trainvars)
+        if len(trainvars) > 10:
+            trainvars = drop_worst_parameters(named_feature_importances)
+        write_new_trainvar_list(trainvars, out_dir)
+    print("\n============ Saving results ================\n")
+    universal.save_results(result_dict, output_dir, plot_extras=True)
+    sm.clear_from_files(global_settings)
+    print("Results saved to " + str(output_dir))
---
+++ scripts/trainvar_opt.py
@@ -0,0 +1,9 @@
+from tthAnalysis.bdtTraining import trainvar_choice as tc
+
+
+def main():
+    tc.run_trainvar_optimization()
+
+
+if __name__ == '__main__':
+    main()
